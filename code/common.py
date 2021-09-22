afd528ac17092556b14fe9e8b269c0859559fe83
Are Frode Kvanum <are.frode.kvanum@gmail.com>
2021-09-22 15:36:25 +0200

    Started on bootstrap for Ridge

--- code/common.py
+++ code/common.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error as MSE
 from sklearn.metrics import r2_score as R2
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Lasso
+import sklearn.linear_model as lm
 from sklearn.utils import resample
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -291,19 +291,13 @@ def SVDinv(A):
     numpy and scipy.linalg at the cost of being slower.
     '''
     U, s, VT = np.linalg.svd(A)
-    """
-    print('test U')
-    print( (np.transpose(U) @ U - U @np.transpose(U)))
-    print('test VT')
-    print( (np.transpose(VT) @ VT - VT @np.transpose(VT)))
-    """
 
     D = np.zeros((len(U),len(VT)))
     D = np.diag(s)
     UT = np.transpose(U); V = np.transpose(VT); invD = np.linalg.inv(D)
-    return np.matmul(V,np.matmul(invD,UT))
+    return V@(invD@UT)
 
-def Bootstrap(x, y, t, maxdegree, n_bootstraps, scale=False, model='Linear', lmb=None):
+def Bootstrap(x, y, t, maxdegree, n_bootstraps, model='Linear', lmb=None):
     for degree in tqdm(range(maxdegree), desc = f"Looping trhough polynomials up to {maxdegree} with {n_bootstraps}: "):
         MSE_test = np.zeros(maxdegree)
         MSE_train = np.zeros(maxdegree)
@@ -316,33 +310,37 @@ def Bootstrap(x, y, t, maxdegree, n_bootstraps, scale=False, model='Linear', lmb
         t_test_ = np.reshape(t_test, newshape=(t_test.shape[0],1))
         t_train_ = np.reshape(t_train, newshape=(t_train.shape[0],1))
 
-        if scale == True:
-            scaler = StandardScaler()
-            scaler.fit(X_train)
-            X_train = scaler.transform(X_train)
-            X_test = scaler.transform(X_test)
+        scaler = StandardScaler()
+        scaler.fit(X_train)
+        X_train_scaled = scaler.transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
 
+        """
         if model == 'Linear':
-            model = LinearRegression()
+            model = lm.LinearRegression()
         elif model == 'Ridge':
             model = RidgeRegression(lmb)
         elif model == 'Lasso':
             model = Lasso(lmb)
+        else:
+            print(f"No valid model was chose, {model} is not a valid model")
+        """
+        model = lm.LinearRegression()
 
         t_pred = np.empty((t_test.shape[0], n_bootstraps))
         t_fit = np.empty((t_train.shape[0], n_bootstraps))
 
         for i in range(n_bootstraps):
-            x_, t_ = resample(X_train, t_train)
-            model.fit(x_, t_)
-            t_pred[:,i] = model.predict(X_test)
-            t_fit[:,i] = model.predict(X_train)
+            x_, t_ = resample(X_train_scaled, t_train)
+            clf = model.fit(x_, t_)
+            t_pred[:,i] = clf.predict(X_test_scaled)
+            t_fit[:,i] = clf.predict(X_train_scaled)
 
-        MSE_test[degree] = np.mean(np.mean((t_test_ - t_pred)**2, axis=1, keepdims=True))
-        MSE_train[degree] = np.mean(np.mean((t_train_ - t_fit)**2, axis=1, keepdims=True))
+        MSE_test[degree] = np.mean( np.mean((t_test_ - t_pred)**2, axis=1, keepdims=True))
+        MSE_train[degree] = np.mean( np.mean((t_train_ - t_fit)**2, axis=1, keepdims=True))
         bias[degree] = np.mean((t_test - np.mean(t_pred, axis=1, keepdims=True))**2)
         variance[degree] = np.mean(np.var(t_pred, axis=1, keepdims=True))
-
+    
     return MSE_test, MSE_train, bias, variance
 
 if __name__ == '__main__':
